46a9c7884bae1f8cd6d03b16d4e3d7125a05ca13
CM\Carl.Leong <carl.leong955@gmail.com>
2026-03-27 09:55:04 +0100

    Basics blockouts finished. Rotating fans and falling platforms.

--- Assets/_Project/Scripts/Obstacles/RotatingObject.cs
+++ Assets/_Project/Scripts/Obstacles/RotatingObject.cs
@@ -10,7 +10,7 @@ public class RotatingObject : MonoBehaviour
 
     public Vector3 Rotation;
     public float Speed;
-    public Collider[] Colliders;
+    //public Collider[] Colliders;
     
     // Update is called once per frame
     void Update()
@@ -18,11 +18,5 @@ public class RotatingObject : MonoBehaviour
         this.transform.Rotate(Rotation * Speed * Time.deltaTime * GameManager.Instance.ObjectsGameSpeed);
     }
 
-    private void OnCollisionEnter(Collision Colliders)
-    {
-        if (Colliders.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("Hit the PLayer");
-        }
-    }
+    
 }
