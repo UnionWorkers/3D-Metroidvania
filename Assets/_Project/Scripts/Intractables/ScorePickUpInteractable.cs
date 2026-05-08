ec16f5cf6e39af4f288c6473e8086c7eddf2d850
Timpan42 <tim@fagerdal.se>
2026-05-08 14:42:42 +0200

    changed score pick up to trigger and not a interacteble

--- Assets/_Project/Scripts/Intractables/ScorePickUpInteractable.cs
+++ Assets/_Project/Scripts/Intractables/ScorePickUpInteractable.cs
@@ -1,23 +1,57 @@
 using Entities.Controller;
-using Interactable;
 using Managers;
+using UnityEngine;
 
-public class ScorePickUpInteractable : BaseInteractable
+public class ScorePickUpInteractable : MonoBehaviour
 {
-    protected override void InteractableAction(PlayerController inPlayerController)
+
+    protected ParticleSystem activeVFX;
+    [SerializeField] protected ParticleSystem interactVFX;
+    [SerializeField] protected ParticleSystem interactableVFX;
+
+    private void Start()
     {
-        if (itemState == ItemState.Destroyed)
+        if (interactableVFX != null && activeVFX == null)
         {
-            return;
+            activeVFX = Instantiate(interactableVFX, transform.position, Quaternion.identity);
+            activeVFX.Play();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                InteractableAction(player);
+            }
         }
 
-        itemState = ItemState.Destroyed;
+    }
 
+    protected virtual void InteractableAction(PlayerController inPlayerController)
+    {
         if (GameManager.Instance != null)
         {
             GameManager.Instance.AddScoreToCounter(1);
         }
 
         Destroy(gameObject);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (activeVFX != null)
+        {
+            Destroy(activeVFX);
+        }
+        if (interactVFX != null)
+        {
+            activeVFX = Instantiate(interactVFX, transform.position, Quaternion.identity);
+            activeVFX.Play();
+            Destroy(activeVFX, 1f);
+        }
     }
 }
